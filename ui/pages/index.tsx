2e597100e5b2affe35430a3e9f2307aba74b6bc2
Synx-x <71853343+Synx-x@users.noreply.github.com>
2022-05-01 13:59:14 +0200

    Fix Testimonal missing

--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -1,7 +1,7 @@
 import Hero from "../components/Hero";
 import NavigationBar from "../components/NavigationBar";
 import VideoGallery from "../components/VideoGallery";
-import Testimonials from "../components/Testimonials";
+import Testimonials from "./../components/Testimonials";
 import { getAllPosts } from "../lib/api";
 import Post from "../types/post";
 type Props = {
