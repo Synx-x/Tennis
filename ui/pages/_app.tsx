d6c26ebaa37d4e1566a1002a20540be3eccbd113
Synx-x <71853343+Synx-x@users.noreply.github.com>
2022-07-15 10:21:41 +0200

    removed backend dependency

--- ui/pages/_app.tsx
+++ ui/pages/_app.tsx
@@ -3,12 +3,12 @@ import "../styles/index.scss";
 import "open-props/style";
 import "open-props/normalize";
 
-import { useApp } from "../lib/gateway"
-import { useEffect } from "react";
+// import { useApp } from "../lib/gateway"
+// import { useEffect } from "react";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    (async () => useApp().load())()
-  }, [])
+  // useEffect(() => {
+  //   (async () => useApp().load())()
+  // }, [])
   return <Component {...pageProps} />
 }
