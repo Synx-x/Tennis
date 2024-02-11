6230c01abacc77731fc6875ecdfcc5571c53f40e
SmallPeenwow <bowdencamrynbrain@gmail.com>
2024-02-11 17:12:01 +0200

    Style Update

--- ui/components/Footer.tsx
+++ ui/components/Footer.tsx
@@ -1,28 +1,30 @@
-import React from "react";
-import styles from "../styles/components/Footer.module.scss";
+import React from 'react';
+import styles from '../styles/components/Footer.module.scss';
 
 const Footer = () => {
-  return (
-    <div className={styles.footerContainer}>
-      <div className={styles.footerInfo}>
-        <h2>Discovering Your True Potential</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam,
-          nulla velit corrupti iusto itaque dolore dolorem sapiente earum fugit
-          sed temporibus atque harum voluptates tenetur.
-        </p>
-      </div>
-      <footer className={styles.footerMain}>
-        <a href="/">Logo</a>
-        <p>© 2022 Stephen Cross. All Rights Reserved. </p>
-        <menu>
-          <li>Facebook</li>
-          <li>Whatsapp</li>
-          <li>Twitter</li>
-        </menu>
-      </footer>
-    </div>
-  );
+	return (
+		<div className={styles.footerParent}>
+			<div className={styles.footerContainer}>
+				<div className={styles.footerInfo}>
+					<h2>Discovering Your True Potential</h2>
+					<p>
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, nulla velit corrupti
+						iusto itaque dolore dolorem sapiente earum fugit sed temporibus atque harum voluptates
+						tenetur.
+					</p>
+				</div>
+				<footer className={styles.footerMain}>
+					<a href='/'>Logo</a>
+					<p>© 2022 Stephen Cross. All Rights Reserved. </p>
+					<menu>
+						<li>Facebook</li>
+						<li>Whatsapp</li>
+						<li>Twitter</li>
+					</menu>
+				</footer>
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
