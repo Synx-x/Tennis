ff1bc8e1b07a675fbc244864dd21c13f3b6bf760
SmallPeenwow <bowdencamrynbrain@gmail.com>
2024-02-16 18:01:44 +0200

    Slider Added Done

--- ui/components/VideoGallery.tsx
+++ ui/components/VideoGallery.tsx
@@ -1,6 +1,44 @@
-import React from "react";
-import styles from "../styles/components/VideoGallery.module.scss";
-import VideoCard from "./VideoCard";
+import React from 'react';
+import styles from '../styles/components/VideoGallery.module.scss';
+import VideoCard from './VideoCard';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+function NextArrow(props: any) {
+	const { className, style, onClick } = props;
+	return <div className={`${styles.nextArrow} ${className}`} style={style} onClick={onClick} />;
+}
+
+function PrevArrow(props: any) {
+	const { className, style, onClick } = props;
+	return <div className={`${styles.prevArrow} ${className}`} style={style} onClick={onClick} />;
+}
+
+const sliderSettings = {
+	dots: false,
+	infinite: false,
+	speed: 700,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	swipeToSlide: true,
+	nextArrow: <NextArrow />,
+	prevArrow: <PrevArrow />,
+	responsive: [
+		{
+			breakpoint: 1550,
+			settings: {
+				slidesToShow: 2,
+			},
+		},
+		{
+			breakpoint: 1100,
+			settings: {
+				slidesToShow: 1,
+			},
+		},
+	],
+};
 
 const VideoGallery = () => {
 	return (
@@ -9,30 +47,40 @@ const VideoGallery = () => {
 				<h2>Redefine what tennis is with the best</h2>
 			</div>
 			<div className={styles.galleryContent}>
-				<VideoCard
-					buttonVisible={true}
-					img={""}
-					title=""
-					description=""
-					onClick={() => window.scrollTo(0, 0)}
-					context="Home"
-				/>
-				<VideoCard
-					buttonVisible={true}
-					img={""}
-					title=""
-					description=""
-					onClick={() => window.scrollTo(0, 0)}
-					context="Home"
-				/>
-				<VideoCard
-					buttonVisible={true}
-					img={""}
-					title=""
-					description=""
-					onClick={() => window.scrollTo(0, 0)}
-					context="Home"
-				/>
+				<Slider {...sliderSettings}>
+					<VideoCard
+						buttonVisible={true}
+						img={''}
+						title=''
+						description=''
+						onClick={() => window.scrollTo(0, 0)}
+						context='Home'
+					/>
+					<VideoCard
+						buttonVisible={true}
+						img={''}
+						title=''
+						description=''
+						onClick={() => window.scrollTo(0, 0)}
+						context='Home'
+					/>
+					<VideoCard
+						buttonVisible={true}
+						img={''}
+						title=''
+						description=''
+						onClick={() => window.scrollTo(0, 0)}
+						context='Home'
+					/>
+					<VideoCard
+						buttonVisible={true}
+						img={''}
+						title=''
+						description=''
+						onClick={() => window.scrollTo(0, 0)}
+						context='Home'
+					/>
+				</Slider>
 			</div>
 		</div>
 	);
